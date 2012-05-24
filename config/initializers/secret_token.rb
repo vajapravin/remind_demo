3740d22bef5f506df825012c01fb514f30baf1d5
jackknife <jackknife@jackknife-Lenovo-S10-3c.(none)>
2012-05-24 21:36:54 +0530

    remind_demo first init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RemindDemo::Application.config.secret_token = 'e6fde22d1d40bc26f7cae26f9a526f7a9bb60e51f1481496a16817c6d29595f262a59b1a90086726c563dae33f41254845008594e189a8f7088f0c1ffd929818'
